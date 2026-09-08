5bec1b9bf661f26f88bb18cb3208f3f1ccab6ad7
Bruno Bastos <b.bastosds@proton.me>
2026-09-08 20:35:45 -0300

    Iniciando o site da iteratecnologia

---
+++ script.js
@@ -0,0 +1,42 @@
+const words = ["Suporte Remoto", "Cloud Computing", "Consultoria de TI", "Infraestrutura"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('typewriter').innerHTML += word.shift();
+        } else {
+            setTimeout(deletingEffect, 2000);
+            return false;
+        }
+        timer = setTimeout(loopTyping, 100); // Velocidade de digitação
+    };
+    loopTyping();
+}
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('typewriter').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            }
+            typingEffect();
+            return false;
+        }
+        timer = setTimeout(loopDeleting, 50); // Velocidade de apagar
+    };
+    loopDeleting();
+}
+
+// Inicia a animação quando a página carrega
+window.onload = function() {
+    typingEffect();
+};
